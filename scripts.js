a313c9705604fef3b92939e3f2bd7f147a3e6df0
viniciussillva98 <viniciussaldanha1998@gmail.com>
2026-03-05 11:29:59 -0300

    Alteração-styles

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 let imagem = document.querySelector(".imagem-cop")
-let circulo =document.querySelector(".circulo")
-    
-function trocaimagem(endereco){
+let circulo = document.querySelector(".circulo")
+
+function trocaimagem(endereco) {
     imagem.src = endereco
 }
-function trocacor(cor){
+function trocacor(cor) {
     circulo.style.background = cor
 }
